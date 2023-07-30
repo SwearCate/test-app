51339c8bf265be67f8ae06716c938a84958be186
SwearCate <94557386+SwearCate@users.noreply.github.com>
2023-07-30 23:25:09 +0300

    Стилизация страницы эпизода
    Есть баг:
    При клике на эпизод в карточке персонажа на странице эпизода URL меняется, но содержимое страницы нет

--- pages/episode/[id].vue
+++ pages/episode/[id].vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <h1>{{ episode.name }}</h1>
-    <p>Дата премьеры: {{ episode.air_date }}</p>
-    <h2>Персонажи:</h2>
-    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <h1 class="text-3xl font-bold">{{ episode.name }}</h1>
+    <p class="text-gray-600 mt-2">Дата премьеры: {{ episode.air_date }}</p>
+    <h2 class="text-2xl font-bold mt-4">Персонажи:</h2>
+    <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4">
       <CharacterCard v-for="character in characters" :key="character.id" :character="character" />
     </div>
   </div>
